45b5230d9f7262b7df5bc3e5d89bef54f36b490e
ybai62868 <ybai62868@gmail.com>
2021-10-18 16:05:32 +0800

    [ADD] Add the tune_model_without_pretrain script

---
+++ scripts/model_tune_without_pretrain.sh
@@ -0,0 +1,1 @@
+python tune_network.py --network mobilenet_v2 --n-trials 100 --cost-model xgb --target "cuda -keys=cudagpu -arch=sm_75 -max_num_threads=1024 -max_threads_per_block=1024 -registers_per_block=65536 -shared_memory_per_block=49152 -thread_warp_size=32"
